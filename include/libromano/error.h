055390b28c1cce157293621f9311a052b4d12c69
Romain Augier <augier.romain@sfr.fr>
2023-04-24 11:14:11 +0200

    Added a new modfule for error checking across various platforms

---
+++ include/libromano/error.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023 - Present Romain Augier */
+/* All rights reserved. */
+
+#pragma once 
+
+#if !defined(__LIBROMANO_ERROR)
+#define __LIBROMANO_ERROR
+
+#include "libromano/libromano.h"
+
+#if defined(ROMANO_WIN)
+#include <Windows.h>
+#elif defined(ROMANO_LINUX)
+#include <errno.h>
+#endif /* defined(ROMANO_WIN) */
+
+ROMANO_CPP_ENTER
+
+ROMANO_FORCE_INLINE int get_last_error(void)
+{
+#if defined(ROMANO_WIN)
+    return GetLastError();
+#elif defined(ROMANO_LINUX)
+    return errno;
+#endif /* defined(ROMANO_WIN) */
+}
+
+ROMANO_CPP_END
+
+#endif /* !defined(__LIBROMANO_ERROR) */
