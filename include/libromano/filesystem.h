99adefe5c4f6e74ac3daf1d16fb05b7bea8ee703
Romain <augier.romain@sfr.fr>
2023-06-12 11:36:14 +0200

    Added some declarations in filesystem, need to implement them now

--- include/libromano/filesystem.h
+++ include/libromano/filesystem.h
@@ -24,6 +24,35 @@ ROMANO_API void fs_parent_dir(const char* path,
 
 ROMANO_API void fs_makedirs(const char* path);
 
+ROMANO_API void fs_chmod(const char* path, 
+                         const uint32_t mode);
+
+ROMANO_API void fs_get_cwd(char* out_path);
+
+ROMANO_API void fs_list_dir(const char* dir_path,
+                           char** out_paths,
+                           uint32_t* count);
+
+ROMANO_API void fs_remove(const char* path);
+
+ROMANO_API void fs_rename(const char* path,
+                          const char* new_name);
+
+ROMANO_API void fs_rm_dir(const char* path);
+
+ROMANO_API uint32_t fs_is_dir(const char* path);
+
+ROMANO_API uint32_t fs_is_file(const char* path);
+
+typedef enum 
+{
+    FsWalkMode_YieldDirs = 0,
+    FsWalkMode_YieldFiles = 1 << 0
+} fs_walk_mode; 
+
+ROMANO_API void fs_walk(const char* path,
+                        const fs_walk_mode mode);
+
 ROMANO_CPP_END
 
 #endif /* __LIBROMANO_FILESYSTEM */
