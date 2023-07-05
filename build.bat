bb00f5d6ec340285d2d6683833a2dece12cdfcc6
Romain <augier.romain@sfr.fr>
2023-07-05 18:15:25 +0200

    Worked more on the buildsystem, added install, configuration and packaging

--- build.bat
+++ build.bat
@@ -12,25 +12,36 @@ set BUILDTYPE=Release
 set RUNTESTS=0
 set REMOVEOLDDIR=0
 set ARCH=x64
+set VERSION=dev
+set INSTALLDIR=%CD%\install
 
 for %%x in (%*) do (
     call :ParseArg %%~x
 )
 
 if %REMOVEOLDDIR% equ 1 (
-    if EXIST build (
+    if exist build (
         call :LogInfo "Removing old build directory"
         rmdir /s /q build
+    )
+    if exist bin (
+        call :LogInfo "Removing old bin directory"
         rmdir /s /q bin
-        rmdir /s /q static
+    )
+    if exist lib (
+        call :LogInfo "Removing old lib directory"
+        rmdir /s /q lib
+    )
+    if exist %INSTALLDIR% (
+        call :LogInfo "Removing old install directory"
+        rmdir /s /q %INSTALLDIR%
     )
 )
 
-if not EXIST bin mkdir bin
+call :LogInfo "Build type: %BUILDTYPE%"
+call :LogInfo "Build version: %VERSION%"
 
-call :LogInfo "Build type : %BUILDTYPE%"
-
-cmake -S . -B build -DRUN_TESTS=%RUNTESTS% -A="%ARCH%" 
+cmake -S . -B build -DRUN_TESTS=%RUNTESTS% -A="%ARCH%" -DVERSION=%VERSION%
 
 if %errorlevel% neq 0 (
     call :LogError "Error caught during CMake configuration"
@@ -41,7 +52,7 @@ cd build
 cmake --build . --config %BUILDTYPE% -j %NUMBER_OF_PROCESSORS%
 
 if %errorlevel% neq 0 (
-    call :LogError "Error caught during compilation"
+    call :LogError "Error caught during CMake compilation"
     cd ..
     exit /B 1
 )
@@ -50,12 +61,20 @@ if %RUNTESTS% equ 1 (
     ctest --output-on-failure
 
     if %errorlevel% neq 0 (
-        call :LogError "Error caught during testing"
+        call :LogError "Error caught during CMake testing"
         type build\Testing\Temporary\LastTest.log
 
         cd ..
         exit /B 1
     )
+)
+
+cmake --install . --config %BUILDTYPE% --prefix %INSTALLDIR%
+
+if %errorlevel% neq 0 (
+    call :LogError "Error caught during CMake installation"
+    cd ..
+    exit /B 1
 )
 
 cd ..
@@ -76,8 +95,39 @@ if "%~1" equ "--export-compile-commands" (
     call :LogWarning "Exporting compile commands is not supported on Windows for now"
 )
 
-exit /B 0
+echo "%~1" | find /I "version">nul && (
+    call :ParseVersion %~1
+)
 
+echo "%~1" | find /I "installdir">nul && (
+    call :ParseInstallDir %~1
+)
+
+exit /B 0
+rem //////////////////////////////////
+
+rem //////////////////////////////////
+rem Little function to parse the version from the command line arg (ex: --version:0.1.3)
+:ParseVersion
+
+for /f "tokens=2 delims=:" %%a in ("%~1") do (
+    set VERSION=%%a
+    call :LogInfo "Version specified by the user: %%a"
+)
+
+exit /B 0
+rem //////////////////////////////////
+
+rem //////////////////////////////////
+rem Little function to parse the install dir from the command line 
+:ParseInstallDir
+
+for /f "tokens=1* delims=:" %%a in ("%~1") do (
+    set INSTALLDIR=%%b
+    call :LogInfo "Install directory specified by the user: %%b"
+)
+
+exit /B 0
 rem //////////////////////////////////
 
 rem //////////////////////////////////
--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@
 # Copyright (c) 2023 - Present Romain Augier 
 # All rights reserved. 
 
-echo Building libromano
+log_info "Building libromano"
 
 BUILDTYPE="Release"
 RUNTESTS=0
 REMOVEOLDDIR=0
 EXPORTCOMPILECOMMANDS=0
+VERSION="dev"
+INSTALLDIR="$PWD/install"
 
+# Little function to parse command line arguments
 parse_args()
 {
     [ "$1" == "--debug" ] && BUILDTYPE="Debug"
@@ -20,6 +23,42 @@ parse_args()
     [ "$1" == "--clean" ] && REMOVEOLDDIR=1
 
     [ "$1" == "--export-compile-commands" ] && EXPORTCOMPILECOMMANDS=1
+
+    [ "$1" == *"version"* ] && parse_version $1
+
+    [ "$1" == *"installdir"* ] && parse_install_dir $1
+}
+
+# Little function to parse the version from a command line argument
+parse_version()
+{
+    arr=(${$1//:/ })
+
+    [ ${#arr[@]} == 2 ] && VERSION=${arr[1]}
+}
+
+# Little function to parse the installation dir from a command line argument
+parse_install_dir()
+{
+    INSTALLDIR="$( cut -d ':' -f 2- <<< "$s" )"
+}
+
+# Little function to log an information message to the console
+log_info()
+{
+    echo "[INFO] : $1"
+}
+
+# Little function to log a warning message to the console
+log_warning()
+{
+    echo "[WARNING] : $1"
+}
+
+# Little function to log an error message to the console
+log_error()
+{
+    echo "[ERROR] : $1"
 }
 
 for arg in "$@"
@@ -27,16 +66,23 @@ do
     parse_args "$arg"
 done
 
+log_info "Build type: $BUILDTYPE"
+log_info "Build version: $VERSION"
+
 if [[ -d "build" && $REMOVEOLDDIR -eq 1 ]]; then
+    log_info "Removing old build directory"
     rm -rf build
-    rm -rf bin
-    rm -rf static
+fi
+
+if [[ -d "install" && $REMOVEOLDDIR -eq 1 ]]; then
+    log_info "Removing old install directory"
+    rm -rf install
 fi
 
 cmake -S . -B build -DRUN_TESTS=$RUNTESTS -DCMAKE_EXPORT_COMPILE_COMMANDS=$EXPORTCOMPILECOMMANDS -DCMAKE_BUILD_TYPE=$BUILDTYPE
 
 if [[ $? -ne 0 ]]; then
-    echo Error during CMake configuration
+    log_error "Error during CMake configuration"
     exit 1 
 fi
 
@@ -45,7 +91,7 @@ cd build
 cmake --build . -- -j $(nproc)
 
 if [[ $? -ne 0 ]]; then
-    echo Error during compilation
+    log_error "Error during CMake build"
     cd ..
     exit 1
 fi
@@ -54,10 +100,18 @@ if [[ $RUNTESTS -eq 1 ]]; then
     ctest --output-on-failure
     
     if [[ $? -ne 0 ]]; then
-        echo Error during testing
+        log_error "Error during CMake testing"
         cd ..
         exit 1
     fi
+fi
+
+cmake --install . --config %BUILDTYPE% --prefix $INSTALLDIR
+
+if [[ $? -ne 0 ]]; then
+    log_error "Error during CMake installation"
+    cd ..
+    exit 1
 fi
 
 cd ..
@@ -65,4 +119,5 @@ cd ..
 if [[ $EXPORTCOMPILECOMMANDS -eq 1 ]]
 then
     cp ./build/compile_commands.json ./compile_commands.json
+    log_info "Copied compile_commands.json to root directory"
 fi
