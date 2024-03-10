9892c687c25c412f9fbcba2ad7e3e6c9efaacef8
Romain Augier <augier.romain@sfr.fr>
2024-03-10 15:27:29 +0100

    Fixed the include in main CMakeLists

---
+++ include/libromano/bool.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023 - Present Romain Augier */
+/* All rights reserved. */
+
+#if !defined(__LIBROMANO_BOOL)
+#define __LIBROMANO_BOOL
+
+#include "libromano/libromano.h"
+
+ROMANO_CPP_ENTER
+
+typedef struct _bool 
+{
+    char data;
+} bool;
+
+ROMANO_CPP_END
+
+#endif /* #define __LIBROMANO_BOOL */
