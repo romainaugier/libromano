8c268a6415bac82824920222955ffcd2b112dcc3
romainaugier <augier.romain@sfr.fr>
2024-03-12 12:44:24 +0100

    Fix for multiple definitions of mem_bswap functions

--- include/libromano/memory.h
+++ include/libromano/memory.h
@@ -29,7 +29,7 @@ ROMANOAPI void debug_free_override(void* ptr,
                                    const char* file);
 
 #define malloc(size) debug_malloc_override(size, __LINE__, __FILE__)
-#define free(ptr) debug_free_override(pt, __LINE__, __FILE__)
+#define free(ptr) debug_free_override(ptr, __LINE__, __FILE__)
 
 #endif /* defined(ROMANO_DEBUG_MEMORY) */
 
@@ -44,13 +44,13 @@ void mem_check_endianness(void);
 ROMANO_API Endianness mem_get_endianness(void);
 
 #if defined(ROMANO_MSVC)
-ROMANO_FORCE_INLINE uint16_t mem_bswapu16(uint16_t x) { return _byteswap_ushort(x); }
-ROMANO_FORCE_INLINE uint32_t mem_bswapu32(uint32_t x) { return _byteswap_ulong(x); }
-ROMANO_FORCE_INLINE uint64_t mem_bswapu64(uint64_t x) { return _byteswap_uint64(x); }
+static ROMANO_FORCE_INLINE uint16_t mem_bswapu16(uint16_t x) { return _byteswap_ushort(x); }
+static ROMANO_FORCE_INLINE uint32_t mem_bswapu32(uint32_t x) { return _byteswap_ulong(x); }
+static ROMANO_FORCE_INLINE uint64_t mem_bswapu64(uint64_t x) { return _byteswap_uint64(x); }
 #elif defined(ROMANO_GCC)
-ROMANO_FORCE_INLINE uint16_t mem_bswapu16(uint16_t x) { return __builtin_bswap16(x); }
-ROMANO_FORCE_INLINE uint32_t mem_bswapu32(uint32_t x) { return __builtin_bswap32(x); }
-ROMANO_FORCE_INLINE uint64_t mem_bswapu64(uint64_t x) { return __builtin_bswap64(x); }
+static ROMANO_FORCE_INLINE uint16_t mem_bswapu16(uint16_t x) { return __builtin_bswap16(x); }
+static ROMANO_FORCE_INLINE uint32_t mem_bswapu32(uint32_t x) { return __builtin_bswap32(x); }
+static ROMANO_FORCE_INLINE uint64_t mem_bswapu64(uint64_t x) { return __builtin_bswap64(x); }
 #endif
 
 ROMANO_API void mem_swap(void *m1, void *m2, const size_t n);
