3391339ffa34dd9fe11e5c87f6c00ee11868dda8
romainaugier <augier.romain@sfr.fr>
2025-01-23 19:14:59 +0100

    Started working on regex

--- src/regex.c
+++ src/regex.c
@@ -2,22 +2,23 @@
 /* Copyright (c) 2023 - Present Romain Augier */
 /* All rights reserved. */
 
+/* https://swtch.com/~rsc/regexp/regexp1.html */
+
 #include "libromano/regex.h"
 
-#define MAX_STATES 32
-#define MAX_SYMBOLS 128
-#define MAX_TRANSITIONS (MAX_STATES * MAX_STATES)
-
-typedef struct 
+typedef struct
 {
-    uint32_t num_states;
-    uint32_t start_state;
-} NFA;
+    int c;
+    State* out;
+    State* out1;
+    int last_list;
+} State;
 
-void nfa_init(NFA* nfa)
+typedef union 
 {
-
-}
+    PtrList* next;
+    State* s;
+} PtrList;
 
 bool regex_compile(Regex* regex, const char* pattern)
 {
