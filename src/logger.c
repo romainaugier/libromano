b395057bcda2635b18e0482c7c30490551fd3864
romainaugier <augier.romain@sfr.fr>
2025-10-23 15:39:05 +0200

    Added thread safety to logger

--- src/logger.c
+++ src/logger.c
@@ -4,6 +4,8 @@
 
 #include "libromano/logger.h"
 #include "libromano/bit.h"
+#include "libromano/atomic.h"
+#include "libromano/thread.h"
 
 #if defined(ROMANO_WIN)
 #include "libromano/time.h"
@@ -22,6 +24,7 @@ typedef enum
     LogMode_File = BIT(1)
 } log_mode;
 
+static volatile int g_logger_lock;
 static volatile int g_logger_initialized = 0;
 static volatile int g_loglevel = LogLevel_Info;
 static volatile int g_log_mode = LogMode_Console;
@@ -30,47 +33,84 @@ static FILE* g_log_file = NULL;
 
 static const char* const levels_as_str[] = { "FATAL", "ERROR", "WARNING", "INFO", "DEBUG" };
 
+void logger_lock_acquire(void)
+{
+    while(!atomic_compare_exchange_32((Atomic32*)&g_logger_lock, 1, 0))
+        thread_yield();
+}
+
+void logger_lock_release(void)
+{
+    atomic_store_32((Atomic32*)&g_logger_lock, 0);
+}
+
 void logger_init(void)
 {
+    logger_lock_acquire();
+
     if(g_logger_initialized)
     {
         return;
     }
 
     g_logger_initialized = 1;
+
+    logger_lock_release();
 }
 
 void logger_set_level(const log_level level)
 {
+    logger_lock_acquire();
+
     g_loglevel = level < 5 ? level : 4;
+
+    logger_lock_release();
 }
 
 void logger_enable_console(void)
 {
+    logger_lock_acquire();
+
     g_log_mode |= LogMode_Console;
+
+    logger_lock_release();
 }
 
 void logger_disable_console(void)
 {
+    logger_lock_acquire();
+
     g_log_mode &= ~LogMode_Console;
+
+    logger_lock_release();
 }
 
 void logger_enable_file(const char* file_path)
 {
+    logger_lock_acquire();
+
     g_log_mode |= LogMode_File;
     g_log_file_path = file_path;
 
     /* _log_file = fopen(_log_file_path, "a"); */
+
+    logger_lock_release();
 }
 
 void logger_disable_file(void)
 {
+    logger_lock_acquire();
+
     g_log_mode &= ~LogMode_File;
+
+    logger_lock_release();
 }
 
 void logger_log(log_level level, const char* format, ...)
 {
     ROMANO_ASSERT(g_logger_initialized, "Logger has not been initialized");
+
+    logger_lock_acquire();
 
     level = level < 5 ? level : 4;
 
@@ -102,7 +142,7 @@ void logger_log(log_level level, const char* format, ...)
             }
 
             fprintf(output_stream,
-                    "[%s] %02d:%02d:%02d:%03ld : %s\n",
+                    "[%s] %02d:%02d:%02d:%03d : %s\n",
                     levels_as_str[level],
                     local_time->tm_hour,
                     local_time->tm_min,
@@ -118,7 +158,7 @@ void logger_log(log_level level, const char* format, ...)
             ROMANO_ASSERT(g_log_file != NULL, "Log file has not been created");
 
             fprintf(g_log_file,
-                    "[%s] %02d:%02d:%02d:%03ld : %s\n",
+                    "[%s] %02d:%02d:%02d:%03d : %s\n",
                     levels_as_str[level],
                     local_time->tm_hour,
                     local_time->tm_min,
@@ -129,10 +169,14 @@ void logger_log(log_level level, const char* format, ...)
             fclose(g_log_file);
         }
     }
+
+    logger_lock_release();
 }
 
 void logger_release(void)
 {
+    logger_lock_acquire();
+
     if(g_log_file != NULL)
     {
         /* fclose(_log_file); */
@@ -140,4 +184,6 @@ void logger_release(void)
 
     if(g_log_mode & LogMode_Console)
         fflush(stdout);
+
+    logger_lock_release();
 }
